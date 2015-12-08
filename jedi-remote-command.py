a8b92874fef72187be8be8e120a8378f8f02b326
NAKAMURA Yoshitaka <arumakanoy@gmail.com>
2015-12-08 21:39:13 +0900

    [WIP] RPC with same external API

---
+++ jedi-remote-command.py
@@ -0,0 +1,104 @@
+'''jedi RPC server
+'''
+import json
+import os
+import sys
+
+# add path for jedi
+sys.path.append(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), 'jedi'))
+
+import jedi
+
+
+class JSONRPC(object):
+    def __init__(self, input=None, output=None):
+        if input is None:
+            input = sys.stdin
+
+        if output is None:
+            output = sys.stdout
+
+        self.input = input
+        self.output = output
+        self._objects = {}
+
+    def run(self):
+        for line in iter(self.input.readline, ''):
+            data = json.loads(line)
+
+            try:
+                func = getattr(self, 'func_' + data['func'])
+                ret = func(*data['args'], **data['kwargs'])
+            except Exception as e:
+                result = {
+                    'code': 'ng',
+                    'exception': repr(type(e)),
+                    'message': str(e),
+                }
+            else:
+                result = {'code': 'ok', 'return': ret}
+
+            self._write_output(result)
+
+    def _write_output(self, data):
+        self.output.write(json.dumps(data, cls=self._json_encoder_factory))
+        self.output.write('\n')
+        self.output.flush()
+
+    def _json_encoder_factory(self, *args, **kwargs):
+        json_encoder = RemoteJSONEncoder(self._objects, *args, **kwargs)
+
+        return json_encoder
+
+    def func_get_from_jedi(self, name):
+        return getattr(jedi, name)
+
+    def func_free(self, id):
+        del self._objects[id]
+
+    def func_remote_object_call(self, id, method, *args, **kwargs):
+        obj = self._objects[id].target
+
+        if method == '__getattr__':
+            return getattr(obj, *args, **kwargs)
+        elif method == '__setattr__':
+            return setattr(obj, *args, **kwargs)
+        elif method == '__call__':
+            return obj(*args, **kwargs)
+        elif method == '__repr__':
+            return repr(obj)
+
+        else:
+            raise NotImplementedError
+
+
+class RemoteJSONEncoder(json.JSONEncoder):
+    def __init__(self, objects, *args, **kwargs):
+        super(RemoteJSONEncoder, self).__init__(*args, **kwargs)
+        self._objects = objects
+
+    def default(self, o):
+        # non JSON encode-able objects are managed on this process.
+        # it stored with unique ID and send this ID to client.
+        # client could access object's attributes through this ID.
+        ref = Ref(o)
+        self._objects[ref.id] = ref
+        # XXX: including builtin-typed attribute may improve
+        #      overall performance, or may not.
+        return {'__type': 'RemoteObject', '__id': ref.id}
+
+
+class Ref(object):
+    '''Reference
+
+    This class has reference for target object and publish unique ID.
+    '''
+    def __init__(self, target):
+        self.target = target
+        self.id = id(self)
+
+
+if __name__ == '__main__':
+    rpc = JSONRPC()
+    rpc.run()
---
+++ jedi_remote.py
@@ -0,0 +1,119 @@
+import json
+import os
+
+import jedi.api
+
+from subprocess import Popen, PIPE
+
+
+class JediRemote(object):
+    '''Jedi remote process communication client
+
+    This class provide jedi compatible API.
+    '''
+    python = 'python'
+    remote_command = 'jedi-remote-command.py'
+
+    def __init__(self, python=None):
+        if python is not None:
+            self.python = python
+
+        self._process = None
+
+    def __del__(self):
+        if self._process is not None:
+            # XXX: why does this raise exception?
+            #self._process.terminate()
+            self._process = None
+
+    @property
+    def process(self):
+        if self._process is None or self._process.poll() is not None:
+            cmd = os.path.join(
+                os.path.dirname(
+                    os.path.abspath(__file__)), self.remote_command)
+            self._process = Popen([self.python, cmd], stdin=PIPE, stdout=PIPE)
+
+        return self._process
+
+    def remote_object_call(self, id, method, *args, **kwargs):
+        return self._call_remote(
+            'remote_object_call', id, method, *args, **kwargs)
+
+    def free(self, id):
+        return self._call_remote('free', id)
+
+    def __getattr__(self, name):
+        return self._call_remote('get_from_jedi', name)
+
+    def _call_remote(self, func, *args, **kwargs):
+        # TODO: may need to serialize RemoteObject
+        output = json.dumps({'func': func, 'args': args, 'kwargs': kwargs})
+
+        self.process.stdin.write(output.encode('utf-8'))
+        self.process.stdin.write(b'\n')
+        self.process.stdin.flush()
+
+        input = json.loads(self.process.stdout.readline().decode('utf-8'),
+                           object_hook=self.remote_object_hook)
+
+        if input.get('code') == 'ok':
+            return input['return']
+
+        elif input.get('code') == 'ng':
+            exception_name = input['exception']
+            message = input['message']
+
+            # try to find from jedi first
+            exception_class = getattr(jedi.api, exception_name, None)
+            if exception_class is None:
+                # ... and then, try to find from builtin
+                import __builtin__
+                exception_class = getattr(__builtin__, exception_name, None)
+
+            if exception_class is None:
+                exception_class = Exception
+                message = '{}: {}'.format(exception_name, message)
+
+            raise exception_class(message)
+
+        else:
+            raise NotImplementedError(repr(input))
+
+    def remote_object_hook(self, obj):
+        if obj.get('__type') == 'RemoteObject':
+            return RemoteObject(self, obj['__id'])
+        else:
+            return obj
+
+    NotFoundError = jedi.api.NotFoundError
+
+
+class RemoteObject(object):
+    '''Remotely managed object
+
+    This class represents objects which managed on remote process.
+    It proxy accesses to remote process.
+    '''
+    def __init__(self, jedi_remote, id):
+        # to bypass this class's __setattr__, use super()
+        super(RemoteObject, self).__setattr__('_jedi_remote', jedi_remote)
+        super(RemoteObject, self).__setattr__('_id', id)
+
+    def __getattr__(self, name):
+        return self._jedi_remote.remote_object_call(
+            self._id, '__getattr__', name)
+
+    def __setattr__(self, name, value):
+        return self._jedi_remote.remote_object_call(
+            self._id, '__setattr__', name, value)
+
+    def __call__(self, *args, **kwargs):
+        return self._jedi_remote.remote_object_call(
+            self._id, '__call__', *args, **kwargs)
+
+    def __repr__(self):
+        return self._jedi_remote.remote_object_call(self._id, '__repr__')
+
+    def __del__(self):
+        self._jedi_remote.free(self._id)
